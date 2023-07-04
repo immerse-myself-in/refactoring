0fe9516d5dabe06798ca556ada1e6391dcfc27e3
hidelos <younbin1103@gmail.com>
2023-07-05 00:09:53 +0900

    feat: 당근클릭 게임 추가(#hidelos)

--- src/counter.ts
+++ src/counter.ts
@@ -1,10 +1,10 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  let test = 'test';
+  let counter = 0;
+
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
 }
